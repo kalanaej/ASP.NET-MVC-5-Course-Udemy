fb70d8b32dec175624f569aaa5bcd92a84784f20
kalanaej <kalanaejayasuriya@gmail.com>
2021-07-28 14:48:21 +0530

    First half of the course

---
+++ Section 5 - Implementing Validation/Vidly/Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        // Section 3
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        // Section 4
+        public DbSet<MembershipType> MembershipType { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
